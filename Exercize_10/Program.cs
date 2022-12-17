90d7919404ee6f66a9c361b6f860c77256b93809
Valentina <valush31@gmail.com>
2022-12-17 19:10:37 +0300

    решение задачи 10: апишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.

---
+++ Exercize_10/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 10. Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int numberOfDigit = DigitNumber(number);
+if (numberOfDigit == 3)
+   {
+    int result = number / 10 % 10; 
+    Console.WriteLine("Вторая цифра числа"+ number + " - это " + result);
+   }
+else
+   {
+    Console.WriteLine("Введенное число - " + number + "не является трехзначным" );
+   }
+
+
+int DigitNumber (int number)
+{
+int numberOfDigit = 0;
+int temp = number;
+while (temp >= 1)
+   {
+    temp = temp / 10;
+    numberOfDigit ++;
+   }
+return numberOfDigit;
+}
